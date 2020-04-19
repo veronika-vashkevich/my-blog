af5d34e72aaf490b486af2d5123dce7df501cf96
veronica.vashkevich <verinica.vashkevich@roxorgaming.com>
2020-04-19 19:39:13 +0100

    First commit

---
+++ src/build/precache-manifest.83550fcfe6987e5320fda6a6f0a932b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b96226c3c85e07c703065c9184ab1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc9271dc8b2551330ff",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "eb21b5816088ede3a3f6",
+    "url": "/static/js/2.8d356d5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d356d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc9271dc8b2551330ff",
+    "url": "/static/js/main.14320679.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
